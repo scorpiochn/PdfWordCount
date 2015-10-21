5a8dacf04dc0f57655fce3d3e25464483aa14d20
scorpiochn <scorpiochn@yeah.net>
2015-10-21 15:20:52 +0800

    fix bug #1

--- src/io/github/scorpiochn/PdfWordCount/WordCountMapper.java
+++ src/io/github/scorpiochn/PdfWordCount/WordCountMapper.java
@@ -1,6 +1,8 @@
 package io.github.scorpiochn.PdfWordCount;
 
 import java.io.IOException;
+import java.util.regex.Matcher;  
+import java.util.regex.Pattern; 
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -10,6 +12,32 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class WordCountMapper extends Mapper<LongWritable,Text, Text, IntWritable> {
 	
+	static Pattern pattern = Pattern.compile("[0-9]+");
+	static Pattern patternL = Pattern.compile("^[^a-z]*");
+	static Pattern patternR = Pattern.compile("[^a-z]*$");
+	
+	private boolean isWord(String w) {
+		Matcher matcher = pattern.matcher(w);
+		if(matcher.find()) {
+			return false;
+		}
+		return true;
+	}
+	
+	private String WordTrimL(String w) {
+		Matcher matcher = patternL.matcher(w);
+		return matcher.replaceAll("");
+	}
+	
+	private String WordTrimR(String w) {
+		Matcher matcher = patternR.matcher(w);
+		return matcher.replaceAll("");
+	}
+	
+	private String WordTrim(String w) {
+		return WordTrimR(WordTrimL(w));
+	}
+	
     @Override
     public void map(LongWritable key, Text value, Context context)
     throws IOException, InterruptedException {
@@ -17,7 +45,11 @@ public class WordCountMapper extends Mapper<LongWritable,Text, Text, IntWritable
         String [] words = line.split(" ");
         for (String w:words) 
         {
-            context.write(new Text(w), new IntWritable(1));
+        	String word = w.toLowerCase();
+        	if(this.isWord(word)) {
+        		word = this.WordTrim(word);
+        		context.write(new Text(word), new IntWritable(1));
+        	}
         }
     }
     
